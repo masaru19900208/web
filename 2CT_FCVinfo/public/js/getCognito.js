af69d5a082c6b273c52c1ce85d1eaac2fb5345d4
masaru_Y <mbdif8@outlook.jp>
2022-05-30 14:13:58 +0900

    hide aws endpoint

--- 2CT_FCVinfo/public/js/getCognito.js
+++ 2CT_FCVinfo/public/js/getCognito.js
@@ -17,7 +17,7 @@ export default {
     //             'Authorization': idToken
     //         }
     //     };
-    //     let url = 'https://t05q426oy4.execute-api.ap-northeast-1.amazonaws.com/default/cognitoLambda';
+    //     let url = 'https://----------------Lambda';
     //     const request = window.axios.create({});
     //     request.get(url, headers)
     //         .then(function (response) {
@@ -31,11 +31,11 @@ export default {
         let accessToken = this.getParameterByHash('access_token');
         //エラーならログインページに遷移
         if (idToken == null || accessToken == null) {
-            window.location.href = 'https://masarutest001.auth.ap-northeast-1.amazoncognito.com/login?client_id=6s0ek26ks3a8ggrn6l9mtqardo&response_type=token&scope=openid&redirect_uri=http://localhost:8080';
+            window.location.href = process.env.VUE_APP_COGNITOPOINT;
         }
         // //ユーザー情報取得
         const requestAws = window.axios.create({});
-        requestAws.get('https://masarutest001.auth.ap-northeast-1.amazoncognito.com/oauth2/userInfo', {
+        requestAws.get(process.env.VUE_APP_COGNITOCHECKPOINT, {
             headers: {
                 Authorization: " Bearer " + accessToken
             }
@@ -51,7 +51,7 @@ export default {
             })
             .catch(function (err) {
                 console.log(err)
-                window.location.href = 'https://masarutest001.auth.ap-northeast-1.amazoncognito.com/login?client_id=6s0ek26ks3a8ggrn6l9mtqardo&response_type=token&scope=openid&redirect_uri=http://localhost:8080';
+                window.location.href = process.env.VUE_APP_COGNITOPOINT;
             })
     },
 
